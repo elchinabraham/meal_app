629bf750f4df805e137baa5f3110b394048a11d7
Ilkin <info@elchinabraham.com>
2022-03-07 14:01:46 +0400

    2022.03.07

---
+++ lib/category_item.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors_in_immutables
+
+import 'package:flutter/cupertino.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color backgroundColor;
+
+  CategoryItem(this.title, this.backgroundColor);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(title),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            backgroundColor.withOpacity(0.7),
+            backgroundColor,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CategoriesScreen(),
+    );
+  }
+}
