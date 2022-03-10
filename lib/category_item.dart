1b7799b9fdcc458ed1cfa6b25e6966712d8f3737
Elchin Abraham <elchinibrahimli8@gmail.com>
2022-03-10 15:06:53 +0400

    2022.03.10 15:06

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:meal_app/category_meals_screen.dart';
+import 'screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
